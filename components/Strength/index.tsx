5374aa43d7c9ee026ee9ef543515888c5b336aca
Minh Le <duyminh.le310@gmail.com>
2022-09-09 19:16:53 -0400

    Strength

--- components/Strength/index.tsx
+++ components/Strength/index.tsx
@@ -1,10 +1,17 @@
 import { FunctionComponent } from "react";
+import styles from './Strength.module.scss';
 
 export const Strength:FunctionComponent = () => {
 
     return (
-        <div>
-            Strength
+        <div className={styles.strength}>
+            <div className={styles.label}>
+                STRENGTH
+            </div>
+            <div className={styles.display}>
+                <span>MEDIUM</span>
+                <div>||||</div>
+            </div>
         </div>
     );
 }
