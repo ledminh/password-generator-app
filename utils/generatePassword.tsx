0649239c54023fa866655a319df300144a630050
Minh Le <duyminh.le310@gmail.com>
2022-09-14 00:37:41 -0400

    Finish Password

--- utils/generatePassword.tsx
+++ utils/generatePassword.tsx
@@ -1,5 +1,22 @@
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTWXYZ';
 
+const shuffle = (password: string):string => {
+    let newPassArr = password.split("");
+
+    for(let i = 0; i < password.length; i++) {
+        const swapI = Math.floor(Math.random()*password.length);
+
+        const temp = newPassArr[i];
+        newPassArr[i] = newPassArr[swapI];
+        newPassArr[swapI] = temp;
+        
+    }
+    
+    console.log(password);
+
+    return newPassArr.join("");
+}
+
 export default function generatePassword(length: number, upper: boolean, lower: boolean, number: boolean, symbols: boolean):string {
     let password = '';
     
@@ -23,6 +40,8 @@ export default function generatePassword(length: number, upper: boolean, lower: 
 
 
     }
+
+    password = shuffle(password);
     
 
     return password;
